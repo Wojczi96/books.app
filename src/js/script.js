d7022fda00dfeb8a8eb8c93c3f703560246f5117
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-11-22 16:49:27 +0100

    Add initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,26 @@
 const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
 const bookListBox = document.querySelector('.books-list');
-  function render(){
-    for(let book in dataSource.books){
-        let generateHTML = template(dataSource.books[book]);
-        let generateDOM = utils.createDOMFromHTML(generateHTML);
-        bookListBox.appendChild(generateDOM);
-    }
+function render(){
+  for(let book in dataSource.books){
+    let generateHTML = template(dataSource.books[book]);
+    let generateDOM = utils.createDOMFromHTML(generateHTML);
+    bookListBox.appendChild(generateDOM);
   }
-  render();
+}
+
+const favoriteBooks = [];
+function initActions(){
+  const images = document.querySelectorAll('.book__image');
+  console.log('images:', images);
+  for(let image of images){
+    image.addEventListener('dblclick', function(event){
+      event.preventDefault();
+      image.classList.add('favorite');
+      console.log('image:', image);
+      const imageId = image.getAttribute('data-id');
+      favoriteBooks.push(imageId);
+    });
+  }
+}
+render();
+initActions();
