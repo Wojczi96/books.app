e5a713becfbaddde6aa15204453c2f3075ba0909
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-11-22 20:51:25 +0100

    Add hidden function

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 const template = Handlebars.compile(document.querySelector('#template-book').innerHTML);
 const bookListBox = document.querySelector('.books-list');
+const filtr = document.querySelector('.filters');
 function render(){
   for(let book in dataSource.books){
     let generateHTML = template(dataSource.books[book]);
@@ -8,7 +9,28 @@ function render(){
   }
 }
 
+const filters = [];
 const favoriteBooks = [];
+
+function filterBooks(){
+  for(let filtrBook of dataSource.books){
+    const bookToHide = document.querySelector('.book__image[data-id="' + filtrBook.id + '"');
+    let booksHidden = false;
+    for(let filtr of filters){
+      if(!filtrBook.details[filtr]){
+        booksHidden = true;
+      }
+    }
+    if(booksHidden){
+      bookToHide.classList.add('hidden');
+    } else {
+      bookToHide.classList.remove('hidden');
+    }
+  }
+}
+
+
+
 function initActions(){
     bookListBox.addEventListener('dblclick', function(event){
       event.preventDefault();
@@ -25,6 +47,21 @@ function initActions(){
         }
       }
   });
+  filtr.addEventListener('click', function(event){
+    const filtrClicked = event.target;
+    if(filtrClicked.tagName === 'INPUT' && filtrClicked.type === 'checkbox' && filtrClicked.name === 'filter'){
+      if(filtrClicked.checked){
+        filters.push(filtrClicked.value);
+        filterBooks();
+      } else {
+        filters.splice(filters.indexOf(filtrClicked.value), 1);
+        filterBooks();
+      }
+    }
+  })
 }
 render();
 initActions();
+
+
+
